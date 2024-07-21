36d2636613b65bd48cdaf9891331ab3351e40526
GeorgeMaged <gogomaged62@gmail.com>
2024-07-21 19:34:51 +0300

    creae table

--- db.sh
+++ db.sh
@@ -82,7 +82,7 @@ list_database() {
     "
     ls -l "$DB_PATH"
 }
-
+#Function to connect to databases
 connect_database(){
     read -p "Select which database you want to connect
     " db_name
@@ -111,8 +111,8 @@ db_menu() {
 
         case $option in 
             1)
-            #create_table
-             echo "Create Table" ;;
+             create_table ;;
+             #echo "Create Table" ;;
             2)
             #list_table
              echo "List Table" ;; 
@@ -141,5 +141,79 @@ db_menu() {
 
 }
 
+create_table()
+{ 
+    local db_name=$1
+    read -p "Name your table: 
+    " table_name
+
+    if [ -f "$DB_DIR/$db_name/$table_name" ]; then
+        echo "Table already exists!
+        "
+        return
+    fi
+
+    read -p "Enter the number of columns : 
+    " num_columns
+    while true; do 
+
+        if [[ "$num_columns" =~ ^[0-9]+$ ]]; then
+            break
+        else
+            echo "Invalid input! Please try again."
+        fi
+    done            
+
+    column_names=()
+    column_types=()
+    column_specials=()
+
+    for ((i=1;i<=$num_columns;i++)); do
+        read -p "Enter name for column number $i: " col_name
+        column_names+=("$col_name")   
+
+      
+        while true; do
+            read -p "Enter datatype for column number $i: 
+               The datatypes available are :
+                  1-string
+                  2-int
+                  3-bool
+                 Enter datatype for column number $i:   " col_type
+
+            case $col_type in
+                1)
+                    column_types+=("string")
+                    break
+                    ;;
+                2)
+                    column_types+=("int")
+                    break
+                    ;;    
+                3)
+                    column_types+=("bool")
+                    break
+                    ;;
+                *)
+                    echo "Not supported datatype right now,Please try again :)" 
+                    ;;
+            esac
+        done    
+        column_types+=("$col_type") 
+    done               
+
+    (
+        IFS=,
+        echo "${column_names[*]}"
+        echo "${column_types[*]}"
+    ) > "$DB_PATH/$db_name/$table_name"
+
+    echo "Table '$table_name' is created successfully! :) 
+    
+    "
+
+    #cat /$DB_PATH/$db_name/$table_name
+
+}
 
 main_menu
